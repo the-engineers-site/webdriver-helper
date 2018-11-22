239c4928587964c817949d14f47ddb3288186566
Yashwant Jagdale <yjagdale@qualys.com>
2018-11-22 10:48:41 +0530

    wait update changed

--- src/main/java/com/automation/finders/ElementFinder.java
+++ src/main/java/com/automation/finders/ElementFinder.java
@@ -1,9 +1,6 @@
 package com.automation.finders;
 
-import com.automation.ElementActionsHelper.WaitUtils;
-import com.automation.browser.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class ElementFinder {
 
@@ -19,4 +16,22 @@ public class ElementFinder {
         String identifier = ".//*[text()='" + text + "']";
         return By.xpath(identifier);
     }
+
+    public static By attributeWithValue(String attribute, String value) {
+        return By.cssSelector("[ " + attribute + "= '" + value + "']");
+    }
+
+    public static By attributeContainsValue(String attribute, String value) {
+        return By.cssSelector("[ " + attribute + "*= '" + value + "']");
+    }
+
+    public static By attributeValueStartWith(String attribute, String value) {
+        return By.cssSelector("[ " + attribute + "^= '" + value + "']");
+    }
+
+    public static By attributeAndParentTag(String parentTag, String attribute, String value) {
+        return By.cssSelector("" + parentTag + "[ " + attribute + "= '" + value + "']");
+    }
+
+
 }
